104d2912812556982d746e7f3a3ce661d5e00f41
Максим <@DESKTOP-T24VRIC>
2023-04-07 10:38:03 +0300

    Добавьте файлы проекта.

---
+++ Arduino/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace Arduino
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+
+        public void my_send(int x)
+        {
+            try
+            {
+                sp.Write($"{x}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        SerialPort sp = new SerialPort();
+        string[] ports = SerialPort.GetPortNames();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            COM.ItemsSource = ports;
+        }
+
+
+
+        private void COM_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            try
+            {
+                if (sp.IsOpen)
+                    sp.Close();
+                else
+                {
+                    //btn.IsEnabled = true;
+                    btn.Visibility = Visibility.Visible;
+                    btn1.Visibility = Visibility.Visible;
+                    btn2.Visibility = Visibility.Visible;
+                    btn3.Visibility = Visibility.Visible;
+                    btn4.Visibility = Visibility.Visible;
+                    btn5.Visibility = Visibility.Visible;
+                    btn6.Visibility = Visibility.Visible;
+                    btn7.Visibility = Visibility.Visible;
+                    btn8.Visibility = Visibility.Visible;
+                    one_clr_screen.Visibility = Visibility.Visible;
+                    one_clr_all.Visibility = Visibility.Visible;
+                    red_sl_.Visibility = Visibility.Visible;
+                    red_sl.Visibility = Visibility.Visible;
+                    blue_sl.Visibility = Visibility.Visible;
+                    green_sl_.Visibility = Visibility.Visible;
+                    green_sl.Visibility = Visibility.Visible;
+                    blue_sl_.Visibility = Visibility.Visible;
+                    bright.Visibility = Visibility.Visible;
+                    bright_one.Visibility = Visibility.Visible;
+                    delay.Visibility = Visibility.Visible;
+                    delay_one.Visibility = Visibility.Visible;
+                }
+                sp.PortName = COM.SelectedItem as string;
+                sp.BaudRate = 9600;
+                sp.Open();
+                
+                //MessageBox.Show("Port is open!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void To_Setting(object sender, RoutedEventArgs e)
+        {
+            SettingWindow setting = new SettingWindow();
+            setting.Show();
+            sp.Close();
+            this.Close();
+            
+        }
+
+
+        private void Button_one_color_all(object sender, RoutedEventArgs e)
+        {
+            my_send(0);
+        }               // 0 
+        private void Button_ukraine(object sender, RoutedEventArgs e)
+        {
+            my_send(1);
+        }                     // 1 
+        private void Button_ukraine_spin(object sender, RoutedEventArgs e)
+        {
+            my_send(2);
+        }                // 2 
+        private void Button_rainbow(object sender, RoutedEventArgs e)
+        {
+            my_send(3);
+        }                     // 3 
+        private void Button_rainbow_2(object sender, RoutedEventArgs e)
+        {
+            my_send(4);
+        }                   // 4 
+        private void Button_rainbow_beatiful(object sender, RoutedEventArgs e)
+        {
+            my_send(5);
+        }            // 5 
+        private void Button_one_color_aroundscreen(object sender, RoutedEventArgs e)
+        {
+            my_send(6);
+        }      // 6 
+        private void Button_rainbow_around(object sender, RoutedEventArgs e)
+        {
+            my_send(7);
+        }              // 7
+        private void Konfeti(object sender, RoutedEventArgs e)
+        {
+            my_send(8);
+        }                            // 8
+        private void Straboskope(object sender, RoutedEventArgs e)
+        {
+            my_send(9);
+        }                        // 9
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            my_send(850);
+        }                              // 85
+
+
+
+        private void Send_colors(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            try
+            {
+                if (red_sl != null && green_sl != null && blue_sl != null && bright != null && delay != null && one_clr_screen != null && one_clr_all != null)
+                {
+                    Color color = Color.FromRgb((byte)red_sl.Value, (byte)green_sl.Value, (byte)blue_sl.Value);
+                    one_clr_screen.Background = new SolidColorBrush(color);
+                    one_clr_all.Background = new SolidColorBrush(color);
+                    
+                    String[] all = { red_sl_.Text.ToString(), green_sl_.Text.ToString(), blue_sl_.Text.ToString(), bright_one.Text.ToString(), delay_one.Text.ToString() };
+                    sp.Write($"65;{all[0]};{all[2]};{all[1]};{all[4]};{all[3]};");
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        private void Send_colors(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (red_sl != null && green_sl != null && blue_sl != null && bright != null && delay != null && one_clr_screen != null && one_clr_all != null)
+                {
+                    Color color = Color.FromRgb((byte)red_sl.Value, (byte)green_sl.Value, (byte)blue_sl.Value);
+                    one_clr_screen.Background = new SolidColorBrush(color);
+                    one_clr_all.Background = new SolidColorBrush(color);
+
+                    String[] all = { red_sl_.Text.ToString(), green_sl_.Text.ToString(), blue_sl_.Text.ToString(), bright_one.Text.ToString(), delay_one.Text.ToString() };
+                    sp.Write($"65;{all[0]};{all[2]};{all[1]};{all[4]};{all[3]};");
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        
+    }
+}
---
+++ Arduino/SettingWindow.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+
+namespace Arduino
+{
+    /// <summary>
+    /// Логика взаимодействия для SettingWindow.xaml
+    /// </summary>
+    /// 
+
+
+
+    public partial class SettingWindow : Window
+    {
+
+
+        SerialPort sp = new SerialPort();
+        string[] ports = SerialPort.GetPortNames();
+
+
+        Dictionary<string, string> my_settings = new Dictionary<string, string>();
+        static string fileContent;
+        
+        public void ReadFileContent()
+        {
+            fileContent = File.ReadAllText(@"D:\ArduinoSetting.txt");
+            char[] delimiterChars = { '/' };
+
+            string[] words = fileContent.Split(delimiterChars);
+
+            for (int i = 0; i < words.Length; i += 2)
+            {
+                my_settings.Add(words[i], words[i + 1]);
+            }
+        }
+
+        public SettingWindow()
+        {   
+
+            InitializeComponent();
+            ReadFileContent();
+
+            try
+            {
+                count.Text = my_settings["LED_COUNT"];
+                pin.Text = my_settings["LED_DT"];
+                unknow.Text = my_settings["NEW"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            COM.ItemsSource = ports;
+
+            grid.Children.Clear();
+
+            int buttonCount = 100; // зчитування кількості кнопок з файлу
+            int rows = 5; // кількість рядків
+            int columns = (int)Math.Ceiling((double)buttonCount / rows); // кількість стовпців
+            
+
+            // додавання стовпців до Grid контейнеру
+            for (int i = 0; i < columns; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            // додавання рядків до Grid контейнеру
+            for (int i = 0; i < rows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            // створення кнопок і додавання їх до Grid контейнеру
+            for (int i = 0; i < buttonCount; i++)
+            {
+
+
+                CheckBox button = new CheckBox();
+                button.Content = $"{(i + 1)}";
+                button.Name = $"btn{(i)}";
+                button.FontSize = 10;   
+                button.Margin = new Thickness(3, 0, 0, 0);
+                button.Padding = new Thickness(0,0,0,0);
+                Color color = Color.FromRgb((byte)0, (byte)255, (byte)255);
+                if (i > 50)
+                {
+                    button.Name = $"btn1";
+                    color = Color.FromRgb((byte)55, (byte)56, (byte)255);
+                }
+                button.Background = new SolidColorBrush(color);
+                button.Foreground = new SolidColorBrush(color);
+                int row = i / columns;
+                int column = i % columns;
+                grid.Children.Add(button);
+                Grid.SetRow(button, row);
+                Grid.SetColumn(button, column);
+            }
+
+        }
+        
+        private void To_Setting(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void create(object sender, RoutedEventArgs e)
+        {
+      
+        }
+
+        private void Clear_choise(object sender, RoutedEventArgs e)
+        {
+            try {
+
+                grid.Children.Clear();
+                int buttonCount = 100; // зчитування кількості кнопок з файлу
+                int rows = 5; // кількість рядків
+                int columns = (int)Math.Ceiling((double)buttonCount / rows); // кількість стовпців
+
+
+
+                // додавання стовпців до Grid контейнеру
+                for (int i = 0; i < columns; i++)
+                {
+                    //grid.ColumnDefinitions.Add(new ColumnDefinition());
+                }
+
+                // додавання рядків до Grid контейнеру
+                for (int i = 0; i < rows; i++)
+                {
+                    //grid.RowDefinitions.Add(new RowDefinition());
+                }
+
+                // створення кнопок і додавання їх до Grid контейнеру
+                for (int i = 0; i < buttonCount; i++)
+                {
+
+
+                    CheckBox button = new CheckBox();
+                    button.Content = $"{(i + 1)}";
+                    button.Name = $"btn{(i)}";
+                    button.FontSize = 10;
+                    button.Margin = new Thickness(3, 0, 0, 0);
+                    button.Padding = new Thickness(0, 0, 0, 0);
+                    button.SetAttribute("Checked", "true");
+                    Color color = Color.FromRgb((byte)0, (byte)255, (byte)255);
+                    if (i > 50)
+                    {
+                        button.Name = $"btn1";
+                        color = Color.FromRgb((byte)55, (byte)56, (byte)255);
+                    }
+                    button.Background = new SolidColorBrush(color);
+                    button.Foreground = new SolidColorBrush(color);
+                    int row = i / columns;
+                    int column = i % columns;
+                    grid.Children.Add(button);
+                    Grid.SetRow(button, row);
+                    Grid.SetColumn(button, column);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void COM_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (sp.IsOpen)
+                    sp.Close();
+                sp.PortName = COM.SelectedItem as string;
+                sp.BaudRate = 9600;
+                sp.Open();
+             }
+             catch (Exception ex)
+             {
+               MessageBox.Show(ex.Message);
+             }
+            }
+
+        private void Send_colors(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+        private void Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Send_colors(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    }
+
